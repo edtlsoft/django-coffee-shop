1a6093a871478bac17e45006d81b7d0e819d22c3
Edward Trigos <edward.trigos@nivelics.co>
2024-11-06 17:48:29 -0500

    feat: add logout and register users

--- coffee_shop/settings.py
+++ coffee_shop/settings.py
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-co'
 
 TIME_ZONE = 'UTC'
 
@@ -134,3 +134,4 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
 CRISPY_TEMPLATE_PACK = "tailwind"
 
 LOGIN_REDIRECT_URL='list_product'
+LOGOUT_REDIRECT_URL='list_product'
--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
+from users.views import RegisterView
 
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name="users.html"), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('registro/', RegisterView.as_view(), name='register'),
 ]
