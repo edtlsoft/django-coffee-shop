9b619f5e463b282e1697257e71cee6f14f1937ae
Edward Trigos <edward.trigos@nivelics.co>
2024-11-09 16:25:31 -0500

    fix: the root url

--- products/urls.py
+++ products/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from products.views import ProductFormView, ProductListView, ProductListApi
 
 urlpatterns = [
+    path('', ProductListView.as_view(), name='list_product'),
     path('add/', ProductFormView.as_view(), name='add_product'),
-    path('list/', ProductListView.as_view(), name='list_product'),
     path('api/', ProductListApi.as_view(), name='api_product'),
 ]
